597e10d022cb2efff55b14623708c1e9e9a2d8f4
mrayhan1 <muhammadrayhan.oe@gmail.com>
2023-05-11 16:10:56 +0600

    second commit: some change has been done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='company-logo'><img src="" alt="" srcset="" /></div>
+            <div className='company-name'>My Fitness club</div>
+        </div>
+    );
+};
+
+export default Header;
