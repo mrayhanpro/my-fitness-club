b8c9f9cdf7d017ff28fa2becd696efdc2b10d862
mrayhan1 <muhammadrayhan.oe@gmail.com>
2023-05-16 22:46:31 +0600

    Cart has been updated. Some styles has been addeded.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ActivityCont from './components/Activity-cont/ActivityCont';
 import Header from './components/Header/Header';
 import ActivityContHeader from './components/ActivityContHeader/ActivityContHeader';
 import activities from './fakeData.json';
+import man from './man.jpg';
 
 function App() {
   // const [activities, setActivities] = useState([])
@@ -27,35 +28,36 @@ function App() {
 
      <div className="cal-cart">
           <div className="member">
-              <div><img src="" alt="" srcset="" /></div>
-              <div>
-                <h3>name</h3>
-                <p>Title</p>
+              <div ><img src={man} alt="Not found."  /></div>
+              <div className='name-n-tittle'>
+                <h2>Jerome J. Diaz</h2>
+                <p> <strong>Frontend Developer</strong> </p>
               </div>
           </div>
           <div className='member-data'>
                 <div>
-                  <p> kg</p>
-                  <p>Weight</p>
+                  <p> <strong>75</strong>kg</p>
+                  <p><strong>Weight</strong></p>
                 </div>
                 <div>
-                  <p></p>
-                  <p>height</p>
+                  <p><strong>5' 7''</strong></p>
+                  <p><strong>Height</strong></p>
                 </div>
                 <div>
-                  <p> yrs</p>
-                  <p>age</p>
+                  <p> <strong>35</strong>yrs</p>
+                  <p><strong>Age</strong></p>
                 </div>
           </div>
-              <h3>Add a break</h3>
+          <h3 className='break-section-header'>Add a break</h3>
           <div className='break-data'>
-                <p>1</p>
-                <p>2</p>
-                <p>3</p>
-                <p>4</p>
-                <p>5</p>
+                <p>5min</p>
+                <p>10min</p>
+                <p>15min</p>
+                <p>20min</p>
+                <p>25min</p>
+                <p>30min</p>
           </div>
-              <h3>Exercise details:</h3>
+              <h3 className='exercise-details'>Exercise details:</h3>
           <div className="exercise-details">
                 <div className='exercise-time-data'>
                   <p>Lorem ipsum dolor sit.</p>
